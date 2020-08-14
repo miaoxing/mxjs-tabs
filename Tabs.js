6f584f635473e0a4e5c453872c03eb55dfb84fb4
twinh <twinhuang@qq.com>
2020-08-14 17:23:35 +0800

    style(eslint): semi: always

--- Tabs.js
+++ Tabs.js
@@ -3,5 +3,5 @@ import {Tabs as RbTabs} from 'react-bootstrap';
 import classNames from 'classnames';
 
 export default ({className, ...props}) => {
-  return <RbTabs className={classNames(className, 'nav-underline header-nav')} {...props}/>
-}
+  return <RbTabs className={classNames(className, 'nav-underline header-nav')} {...props}/>;
+};
